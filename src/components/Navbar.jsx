afa6540d3e6c1a211a8bb1b8159c3b1ac44cfc77
anidisamuel2006@gmail.com <anidisamuel2006@gmail.com>
2024-10-27 01:27:50 -0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "../assets/css/navbar.css";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar-logo">
+          <h1>Anidi</h1>
+          <p>All Tech Solution</p>
+        </div>
+        <ul className="navbar-links">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About me</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+        <button className="navbar-button">Hire Me</button>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar
